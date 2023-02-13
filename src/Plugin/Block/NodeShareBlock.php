b2468d541957bb6e9064232d12687be6d34b3e65
Andrey Zbukar <andrey.zbk@gmail.com>
2023-02-13 15:06:35 +0200

    Moved css/scss to assets, added common LB Blocks - Share and Tags

---
+++ src/Plugin/Block/NodeShareBlock.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\y_lb\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides Share block.
+ *
+ * @Block(
+ *   id = "lb_node_share",
+ *   admin_label = @Translation("Share Block"),
+ *   category = @Translation("Common blocks")
+ * )
+ */
+class NodeShareBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The route provider.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $currentRouteMatch;
+
+  /**
+   * Cuurent node.
+   *
+   * @var \Drupal\node\Entity\Node
+   */
+  protected $node;
+
+  /**
+   * Constructs a new NodeShareBlock instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteProviderInterface $route_provider
+   *   The route provider.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CurrentRouteMatch $currentRouteMatch) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentRouteMatch = $currentRouteMatch;
+    $this->node = $currentRouteMatch->getParameter('node');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    if (!$this->node) {
+      return [];
+    }
+
+    $url = urlencode($this->node->toUrl('canonical', ['absolute' => TRUE])->toString());
+
+    $build = [
+      '#links' => [
+        'facebook' => 'https://www.facebook.com/sharer.php?u='. $url,
+        'twitter'  => 'https://www.linkedin.com/shareArticle?mini=true&url=' . $url,
+        'linkedin' => 'https://twitter.com/intent/tweet?url=' . $url,
+      ]
+    ];
+    return $build;
+  }
+
+}
