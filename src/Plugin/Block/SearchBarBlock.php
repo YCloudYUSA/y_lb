33f3882eb9509ec4a5492391737d73551cd94aed
Alexey Aleev <aleevas@gmail.com>
2023-05-12 12:33:15 +0200

    [DS-287] Add Header Section for the CT Landing Page (Layout Builder)

---
+++ src/Plugin/Block/SearchBarBlock.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\y_lb\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides the Search Bar block.
+ *
+ * @Block(
+ *   id = "ws_search_bar",
+ *   admin_label = @Translation("Search Bar Block"),
+ *   category = @Translation("Common blocks")
+ * )
+ */
+class SearchBarBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Contains the configuration object factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a new WebsiteNameBlock instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $build['display_search'] = theme_get_setting('display_search_form');
+    $build['search_key'] = theme_get_setting('search_query_key');
+    $search_alias = 'internal:/' . theme_get_setting('search_page_alias');
+    $build['search_results_path'] = Url::fromUri($search_alias)->toString();
+
+    return $build;
+  }
+
+}
---
+++ src/Plugin/Block/SiteNameBlock.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\y_lb\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides the Website name block.
+ *
+ * @Block(
+ *   id = "ws_site_name",
+ *   admin_label = @Translation("Website Name Block"),
+ *   category = @Translation("Common blocks")
+ * )
+ */
+class SiteNameBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Contains the configuration object factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a new WebsiteNameBlock instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $build['site_name'] = $this->configFactory->get('system.site')->get('name');
+    $build['front_page'] = Url::fromRoute('<front>')->toString();
+
+    return $build;
+  }
+
+}
