c966cef52fd37386721233301334c11e69fde871
Alexey Aleev <aleevas@gmail.com>
2023-06-20 16:30:30 +0200

    [DS-784] Fix scroll for the mobile menu on small screens

--- assets/js/header.js
+++ assets/js/header.js
@@ -21,6 +21,9 @@
           body.css('overflow', 'auto');
           submenu.removeClass('open');
           userMenu.removeClass('container');
+          $('.header--bottom').css('position', 'absolute');
+          header.css('overflow', 'auto');
+          $('a.highlighted').show();
         } else {
           header.addClass('open');
           userMenu.addClass('container');
@@ -39,8 +42,6 @@
         }
         switchMainMenuType();
       });
-      // Hide main menu until page is ready.
-      header.find('nav').css('visibility', 'visible');
       // A fix for a small mobile screen.
       header.scroll(() => {
         if (header.scrollTop() > 0) {
@@ -50,11 +51,16 @@
         }
       });
 
+      // Hide main menu items until page is ready.
+      header.find('nav').css('visibility', 'visible');
+      header.find('.block-ws-search-bar').css('visibility', 'visible');
+      header.find('.block-ws-site-logo').css('visibility', 'visible');
+
       function switchMainMenuType() {
         const header = $('.ws-header');
         // This size is can be fixed.
-        const logo =  220;
-        var main_menu = document.querySelector('.header--bottom-middle-column');
+        const logo =  230;
+        var main_menu = document.querySelector('.ws-main-menu-wrapper');
         var right_menu = document.querySelector('.header--bottom-right-column');
         var max_available_width = $(window).width() - logo - right_menu.offsetWidth;
 
@@ -117,7 +123,12 @@
             subMenu.removeClass('open');
           } else {
             subMenu.addClass('open');
+            $('a.highlighted').hide();
           }
+          if (header.hasClass('scrolled')) {
+            $('.header--bottom').css('position', 'fixed');
+          }
+          header.css('overflow-y', 'hidden');
         }
         const secondLevelDropdownLink = $(this).parent().find('li.active');
         secondLevelDropdownLink.each(function () {
@@ -129,6 +140,9 @@
         const back_1_level = $(this, context).parent().parent().parent().parent().parent();
         if (back_1_level.hasClass('open')) {
           back_1_level.removeClass('open');
+          $('a.highlighted').show();
+          $('.header--bottom').css('position', 'absolute');
+          header.css('overflow-y', 'auto');
         }
         if (back_1_level.hasClass('show')) {
           back_1_level.removeClass('show');
@@ -150,6 +164,9 @@
           $('.dropdown').removeClass('show');
         }
       });
+      // Set top position for submenus in mobile screen.
+      const mobileHeaderHeight = $('.header--bottom').outerHeight();
+      $('.header-nav__submenu').css('top', mobileHeaderHeight + 'px');
     }
   };
 })(jQuery, Drupal);
