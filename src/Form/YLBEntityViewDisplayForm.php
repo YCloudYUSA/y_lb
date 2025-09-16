fe9f6f40fbd207b2ae2d688d5b11ec63fc867260
Mário Reis <mario@imagexmedia.com>
2025-09-16 15:04:28 -0300

    Add an option to control if the styles can be overriden on each node.

---
+++ src/Form/YLBEntityViewDisplayForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\y_lb\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\layout_builder\Entity\LayoutEntityDisplayInterface;
+use Drupal\layout_builder\Form\LayoutBuilderEntityViewDisplayForm;
+
+/**
+ * Provides a custom form for the Y Layout Builder settings for entity display.
+ */
+class YLBEntityViewDisplayForm extends LayoutBuilderEntityViewDisplayForm {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function form(array $form, FormStateInterface $form_state) {
+    if ($this->isCanonicalMode($this->entity->getMode())) {
+      $entity_type = $this->entityTypeManager->getDefinition($this->entity->getTargetEntityTypeId());
+      $form = parent::form($form, $form_state);
+      $form['layout']['allow_style'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Allow each @entity to have its style customized.', [
+          '@entity' => $entity_type->getSingularLabel(),
+        ]),
+          '#default_value' => $this->entity->getThirdPartySetting('y_lb', 'allow_style', FALSE),
+      ];
+    }
+    return $form;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function entityFormEntityBuild($entity_type_id, LayoutEntityDisplayInterface $display, &$form, FormStateInterface &$form_state) {
+    parent::entityFormEntityBuild($entity_type_id, $display, $form, $form_state);
+    $allow_style = (bool) $form_state->getValue(['layout', 'allow_style'], FALSE);
+    $display->setThirdPartySetting('y_lb', 'allow_style', $allow_style);
+  }
+
+}
